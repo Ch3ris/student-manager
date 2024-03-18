7b4952b6d3cb617282999823be505fdd2da84feb
VictorSimon2003 <simon.victoras@gmail.com>
2024-03-18 18:57:54 +0200

    SM-1 Added the login screen

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module com.example.demo {
     requires javafx.fxml;
 
 
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
+    opens loose.oose.fis.lab.student.manager to javafx.fxml;
+    exports loose.oose.fis.lab.student.manager;
 }
